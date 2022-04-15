4cc960b7e5279e6eee5dd511a8c93a2ca91d431a
Kerem KORKMAZ <korkmazkerem94@gmail.com>
2022-04-15 17:33:21 +0300

    16. Gün ders sonu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nortwind';
+  title: string = 'nortwind';
+  user: string = "Kerem KORKMAZ";
+  
+
 }
