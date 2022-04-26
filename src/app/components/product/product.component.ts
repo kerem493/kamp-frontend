25ac45b00331eca5ceb35309326e7c9cd84735d0
Kerem KORKMAZ <korkmazkerem94@gmail.com>
2022-04-26 22:20:15 +0300

    19. gün ders sonu (26.04.2022)

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,9 +14,11 @@ export class ProductComponent implements OnInit {
 
   products:Product [] = [];
   dataLoaded = false;
+  filterText="";
  
   constructor(private productService:ProductService, 
-    private activatedRoute:ActivatedRoute) { }
+    private activatedRoute:ActivatedRoute,
+    private toastrService:ToastrService, private cartService:CartService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
@@ -39,6 +43,11 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product:Product){
+    this.toastrService.success("Sepete eklendi",product.productName);
+    this.cartService.addToCart(product);
   }
 
 
